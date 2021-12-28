219e49912f81258bb13b41c1bcebf5bc19308116
federico.fortuna <fedekpg17@gmail.com>
2021-12-28 12:52:22 -0300

    Proyecto

---
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Indica a Spring que esta es la clase que debe ejecutar
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
